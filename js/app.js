c49e1c8da81ad51d12647187ac6553e2af469e53
GabrielVRM <Gabriel.vieira2595@gmail.com>
2022-06-08 21:26:58 -0300

    button reload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const Mario = document.querySelector(".mario");
 const Pipe = document.querySelector(".pipe");
+const reload = document.querySelector(".button")
 
 const Jump = () => {
   Mario.classList.add("pular");
@@ -10,18 +11,30 @@ const Jump = () => {
 };
 const loop = setInterval(() => {
   const pipePosition = Pipe.offsetLeft;
-  const marioPisition = +window.getComputedStyle(Mario).bottom.replace('px', '');
-  console.log(marioPisition)
-if (pipePosition <= 64 && pipePosition > 0 && marioPisition <70) {
+  const marioPisition = +window
+    .getComputedStyle(Mario)
+    .bottom.replace("px", "");
+  console.log(marioPisition);
+
+
+  //eu que fiz 
+const gameReload = window.getComputedStyle(reload).display
+
+  if (pipePosition <= 64 && pipePosition > 0 && marioPisition < 70) {
     Pipe.style.animation = "none";
     Pipe.style.left = `${pipePosition}px`;
 
-    Mario.style.animation ="none";
+    Mario.style.animation = "none";
     Mario.style.bottom = `${marioPisition}px`;
 
-    Mario.src= './img/game-over.png' 
-    Mario.style.width = '75px'
+    Mario.src = "./img/game-over.png";
+    Mario.style.width = "75px";
+
+    reload.style.display='flex'
+
   }
+
+
 }, 10);
 
 document.addEventListener("keydown", Jump);
